4b88e0070b016619460e7b548f2e321928efd558
poy <ghhh7731@gmail.com>
2023-12-24 20:01:53 +0700

    ci: :memo: add new code createPhoneNumber & test

--- codewars/create_phone_number.test.ts
+++ codewars/create_phone_number.test.ts
@@ -1,1 +1,6 @@
-//demo5
+import { expect, test } from "bun:test";
+import { createPhoneNumber } from "./create_phone_number";
+
+test("Should return the smallest int 2", () => {
+	expect(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual("(123) 456-7890");
+});
--- codewars/create_phone_number.ts
+++ codewars/create_phone_number.ts
@@ -1,1 +1,12 @@
-//demo5
+export function createPhoneNumber(numbers: number[]): string {
+	if (numbers.length !== 10) {
+		throw new Error("จำนวนสมาชิกไม่เท่ากับ 10 หรือไม่ โปรดตรวจสอบ");
+	}
+
+	const areaCode = numbers.slice(0, 3).join("");
+	const firstPart = numbers.slice(3, 6).join("");
+	const secondPart = numbers.slice(6, 10).join("");
+
+	return `(${areaCode}) ${firstPart}-${secondPart}`;
+}
+console.log(createPhoneNumber([5, 2, 6, 7, 3, 9, 6, 0, 8, 3]));
