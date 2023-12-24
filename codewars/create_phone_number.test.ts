677a496466ffe144d48fadb37d31566f44c402f4
poy <ghhh7731@gmail.com>
2023-12-24 20:24:21 +0700

    ci: :memo: add new test createPhoneNumber

--- codewars/create_phone_number.test.ts
+++ codewars/create_phone_number.test.ts
@@ -4,3 +4,11 @@ import { createPhoneNumber } from "./create_phone_number";
 test("Should return the (123) 456-7890", () => {
 	expect(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual("(123) 456-7890");
 });
+
+test("Should return the (062) 116-7145", () => {
+	expect(createPhoneNumber([0, 6, 2, 1, 1, 6, 7, 1, 4, 5])).toEqual("(062) 116-7145");
+});
+
+test("Should return the จำนวนสมาชิกไม่เท่ากับ 10 หรือไม่ โปรดตรวจสอบ ", () => {
+	expect(createPhoneNumber([2, 3, 4, 5, 6])).toEqual("จำนวนสมาชิกไม่เท่ากับ 10 หรือไม่ โปรดตรวจสอบ");
+});
